5858a393e4155754722bb3060d9b12f58b325bb1
Travis Dalton <dalton.travis@gmail.com>
2019-12-27 18:13:17 -0500

    added first test question

---
+++ STORE.js
@@ -0,0 +1,94 @@
+const STORE = {
+    questions: [
+        //1
+        {
+            question: "Modern golf was popularized in around the 15th century, in which country?",
+            options: [
+                "Ireland",
+                "England", 
+                "Scotland",
+                "America"
+            ],
+            answer: "Scotland"
+        },
+        //2
+        {
+            question: "If you take a re-do on your shot, this is called what?",
+            options: [
+                "Cheating",
+                "Fore",
+                "Do-over",
+                "Mulligan"
+            ],
+            answer: "Mulligan"
+        },
+        //3
+        {
+            question: "What do you call the predetermined number of shots it would take a scratch golfer to complete a hole, round, or tournament?",
+            options: [
+                "Par",
+                "Fore",
+                "Birdie",
+                "Unattainable"
+            ],
+            answer: "Par"
+        },
+        //4
+        {
+            question: "What is it called when someone scores one under par on a hole?",
+            options: [
+                "Eagle",
+                "Birdie",
+                "Dove",
+                "Osprey"
+            ],
+            answer: "Birdie"
+        },
+        //5
+        {
+            question: "When a group reaches the green, who gets to putt first?",
+            options: [
+                "The player closest to the hole",
+                "The player furthest from the hole",
+                "The player that reached the green first",
+                "The player that reached the green last"
+            ],
+            answer: "The player furthest from the hole"
+        },
+        //6
+        {
+            question: "Which of the following is not a Major?",
+            options: [
+                "The Masters",
+                "The Canadian Open",
+                "The US Open",
+                "The Open"
+            ],
+            answer: "The Canadian Open"
+        },
+        //7
+        {
+            question: "Which of the following handicaps indicates the best golfer?",
+            options: [
+                "0",
+                "12",
+                "18",
+                "27"
+            ],
+            answer: "0"
+        },
+        //8
+        {
+            question: "Who is the winningest golfer of all time?",
+            options: [
+                "Sam Snead",
+                "Tiger Woods",
+                "Sam Snead and Tiger Woods are tied",
+                "Neither Sam Snead nor Tiger Woods"
+            ],
+            answer: "Sam Snead and Tiger Woods are tied"
+        }
+    ],
+    currentQuestion: 0,
+    score: 0
+};
